a5cba6a51ec713f80ad54ed1561615b84abcba32
huangwen@bjlb999.onexmail.com <huangwen@bjlb999.onexmail.com>
2023-11-21 09:40:25 +0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+// import  * as React from 'react'
+import {createRoot} from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+const root = createRoot(document.getElementById('root'));
+// console.log("root",root);
+
+// const element = <div><span>hello world</span>123</div> 
+// console.log("element",element);
+
+//  root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+debugger;
+root.render("hello world")
